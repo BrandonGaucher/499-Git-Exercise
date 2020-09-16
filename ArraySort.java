6761c17cde29714bcebd3e075ac72b2e2d4a014b
BrandonGaucher <brandonjgaucher@gmail.com>
2020-09-15 21:42:47 -0700

    Update ArraySort.java
    updated string sort

--- ArraySort.java
+++ ArraySort.java
@@ -4,10 +4,31 @@ import java.util.Arrays;
 public class ArraySort {
     public static void main(String[] args) {
 
-           // variables
-           int[] numArray = { 2, 9, -7, 100, 40, 5 };
-           String[] stringArray = { "Brandon", "Zoe", "Joe", "Carl" };
-           int temp;
+        int[] numArray = { 2, 9, -7, 100, 40, 5 };
+        int temp;
+        String[] stringArray = { "Brandon", "Zoe", "Joe", "Carl" };
+
+        // sorting integer
+
+        System.out.println("the original integer  array is: ");
+        for (int num : numArray) {
+            System.out.println(num + " ");
+
+        }
+        for (int i = 0; i < numArray.length; i++) {
+            for (int j = i + 1; j < numArray.length; j++) {
+                if (numArray[i] > numArray[j]) {
+                    temp = numArray[i];
+                    numArray[i] = numArray[j];
+                    numArray[j] = temp;
+                }
+            }
+        }
+        System.out.print("Array Elements in Ascending Order: ");
+        for (int i = 0; i < numArray.length - 1; i++) {
+            System.out.print(numArray[i] + ", ");
+        }
+        System.out.print(numArray[numArray.length - 1]);
 
         // sorting string array.
 
@@ -20,8 +41,8 @@ public class ArraySort {
         System.out.println("The sorted integer Array is: ");
         for (String str : stringArray) {
             System.out.println(str + " ");
-        }
-        // sorting string array.
+            
+            // sorting string array.
 
         System.out.println("the original integer  array is: ");
         for (String str : stringArray) {
@@ -33,6 +54,8 @@ public class ArraySort {
         for (String str : stringArray) {
             System.out.println(str + " ");
         }
+        }
+
     }
 
 }
