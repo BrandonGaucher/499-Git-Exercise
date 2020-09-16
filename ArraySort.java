76defec14883b3fd5d5fbd4ec991f35a9482a5ce
BrandonGaucher <brandonjgaucher@gmail.com>
2020-09-15 21:40:29 -0700

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -4,8 +4,23 @@ import java.util.Arrays;
 public class ArraySort {
     public static void main(String[] args) {
 
-        String[] stringArray = { "Brandon", "Zoe", "Joe", "Carl" };
+           // variables
+           int[] numArray = { 2, 9, -7, 100, 40, 5 };
+           String[] stringArray = { "Brandon", "Zoe", "Joe", "Carl" };
+           int temp;
 
+        // sorting string array.
+
+        System.out.println("the original integer  array is: ");
+        for (String str : stringArray) {
+            System.out.println(str + " ");
+
+        }
+        Arrays.sort(stringArray);
+        System.out.println("The sorted integer Array is: ");
+        for (String str : stringArray) {
+            System.out.println(str + " ");
+        }
         // sorting string array.
 
         System.out.println("the original integer  array is: ");
